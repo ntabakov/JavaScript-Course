8edffb16ce99c527450fc8c87d5e23d6fe128630
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-11-16 13:37:40 +0200

    add homework

---
+++ JavaScript-Advanced/DOM Introduction/01. Subtraction/subtract.js
@@ -0,0 +1,4 @@
+function subtract() {
+    let firstNumber = document.getElementById('firstNumber').value;
+    console.log(firstNumber);
+}
---
+++ JavaScript-Advanced/DOM Introduction/03. Accordion/accordion.js
@@ -0,0 +1,15 @@
+function toggle() {
+    
+    let button = document.getElementsByClassName('button')[0];
+    let extra = document.getElementById('extra');
+    if(button.textContent == 'More'){
+        button.textContent = 'Less';
+        extra.style.display = 'block';
+    }
+    else{
+        button.textContent = 'More';
+        extra.style.display = 'none';
+
+    }
+    console.log(button);
+}
