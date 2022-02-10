0b9ca99e97659e4007ed10ddb098068540a95326
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2022-02-10 09:46:34 +0200

    .

--- JavaScript-Advanced/DOM Manipulations and Events/02. Time-Converter/solution.js
+++ JavaScript-Advanced/DOM Manipulations and Events/02. Time-Converter/solution.js
@@ -1,4 +1,42 @@
 function attachEventsListeners() {
+    const ratios = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400,
+    }
 
-    console.log('TODO:...');
+    function convert(value, unit) {
+        const timeInDays = value / ratios[unit];
+        return {
+            days: timeInDays,
+            hours: timeInDays * ratios.hours,
+            minutes: timeInDays * ratios.minutes,
+            seconds: timeInDays * ratios.seconds,
+        }
+    }
+
+    const daysInput = document.getElementById('days');
+    const hoursInput = document.getElementById('hours');
+    const minutesInput = document.getElementById('minutes');
+    const secondsInput = document.getElementById('seconds');
+
+    document.getElementById('daysBtn').addEventListener('click', onConvert);
+    document.getElementById('hoursBtn').addEventListener('click', onConvert);;
+    document.getElementById('minutesBtn').addEventListener('click', onConvert);;
+    document.getElementById('secondsBtn').addEventListener('click', onConvert);;
+
+    function onConvert(e) {
+        const input = e.target.parentElement.children[1];
+
+        const time = convert(Number(input.value), input.id);
+
+        console.log(time);
+
+        daysInput.value = time.days;
+        hoursInput.value = time.hours;
+        minutesInput.value=time.minutes;
+        secondsInput.value=time.seconds;
+    }
+
 }
--- JavaScript-Advanced/DOM Manipulations and Events/03. Locked-Profile/app.js
+++ JavaScript-Advanced/DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    // const a = document.querySelectorAll('input[name="user1Locked"]');
+    // console.log(a);
+    const buttons = Array.from(document.getElementsByTagName('button'));
+    buttons.forEach(x =>
+        x.addEventListener('click', revealOrHide)
+    );
+
+    function revealOrHide(e) {
+        const div = e.target.parentElement.getElementsByTagName('div')[0];
+        let radio = e.target.parentElement.querySelector('input[type="radio"]');
+        
+        if (!radio.checked) {
+
+            if (e.target.textContent == 'Hide it') {
+                e.target.textContent = 'Show more';
+                div.style.display = '';
+            } else {
+                e.target.textContent = 'Hide it'
+                div.style.display = 'block';
+
+            }
+        }
+    }
 }
